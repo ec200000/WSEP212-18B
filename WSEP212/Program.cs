4dd4e80006212b29d6cb8977db6c39dc20bbd910
ec200000 <ec200000@gmail.com>
2021-03-24 22:16:26 +0200

    first commit

---
+++ WSEP212/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WSEP212
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
