1379bfa08b46cef7883518525fb69342fb855420
iris kronfeld <iris.kronfeld@gmail.com>
2021-03-31 11:09:47 +0300

    user changed

--- WSEP212/DomainLayer/User.cs
+++ WSEP212/DomainLayer/User.cs
@@ -14,30 +14,148 @@ namespace WSEP212.DomainLayer
         public ShoppingCart shoppingCart { get; set; }
         public ConcurrentBag<PurchaseInfo> purchases { get; set; }
         public ConcurrentBag<SellerPermissions> sellerPermissions { get; set; }
+        public String address { get; set; }
 
         public void changeState(UserState state);
 
-        public bool register(String userName, String password);
-        public bool login(String userName, String password);
-        public bool logout(String userName);
+        // params: string username, string password
+        // returns: bool
+        public void register(Object list)
+        {
+            ThreadParameters param = (ThreadParameters)list;
+            String username = (String)param.parameters[0];
+            String password = (String)param.parameters[1];
+            bool res = state.register(username, password);
+            param.result = res;
+            param.eventWaitHandle.Set(); // signal we're done
+        }
 
-        public bool addItemToShoppingCart(int storeID, int itemID);
-        public bool removeItemFromShoppingCart(int storeID, int itemID);
+        // params: string username, string password
+        // returns: bool
+        public void login(Object list)
+        {
+            return state.login(userName, password);
+        }
+
+        // params: string username
+        // returns: bool
+        public void logout(Object list)
+        {
+            return state.logout(userName);
+        }
+
+        // params: int storeID, int itemID
+        // returns: bool
+        public void addItemToShoppingCart(Object list)
+        {
+            return state.addItemToShoppingCart(storeID, itemID);
+        }
+
+        // params: int storeID, int itemID
+        // returns: bool
+        public void removeItemFromShoppingCart(Object list)
+        {
+            return state.removeItemFromShoppingCart(storeID, itemID);
+        }
         //edit item in shopping cart is equal to -> remove + add
-        public bool purchaseItems(); //later
-        public bool openStore(String storeName, PurchasePolicy purchasePolicy, SalesPolicy salesPolicy);
-        public bool itemReview(String review, int itemID, int storeID);
-        public bool addItemToStorage(int storeID, Item item, int quantity);
-        public bool removeItemFromStorage(int storeID, Item item);
-        public bool editItemDetails(int storeID, Item item);
-        public bool appointStoreManager(String managerName, int storeID); //the store manager will receive default permissions(4.9)
-        public bool appointStoreOwner(String storeOwnerName, int storeID);
-        public bool editManagerPermissions(String managerName, ConcurrentBag<Permissions> permissions);
-        public bool removeStoreManager(String managerName, int storeID);
-        public ConcurrentDictionary<String, ConcurrentBag<Permissions>> getOfficialsInformation(int storeID);
-        public ConcurrentBag<PurchaseInfo> getStorePurchaseHistory(int storeID); //all the purchases of the store that I manage/own
-        public ConcurrentDictionary<String, ConcurrentBag<PurchaseInfo>> getUsersPurchaseHistory();
-        public ConcurrentDictionary<int, ConcurrentBag<PurchaseInfo>> getStoresPurchaseHistory();
+
+        // params: ?
+        // returns: bool
+        public void purchaseItems(Object list) //later
+        {
+            return state.purchaseItems();
+        }
+
+        // params: String storeName, PurchasePolicy purchasePolicy, SalesPolicy salesPolicy
+        // returns: bool
+        public void openStore(Object list)
+        {
+            return state.openStore(storeName, purchasePolicy, salesPolicy);
+        }
+
+        // params: String review, int itemID, int storeID
+        // returns: bool
+        public void itemReview(Object list)
+        {
+            return state.itemReview(review, itemID, storeID);
+        }
+
+        // params: int storeID, Item item, int quantity
+        // returns: bool
+        public void addItemToStorage(Object list)
+        {
+            return state.addItemToStorage(storeID, item, quantity);
+        }
+
+        // params: int storeID, Item item
+        // returns: bool
+        public void removeItemFromStorage(Object list)
+        {
+            return state.removeItemFromStorage(storeID, item);
+        }
+
+        // params: int storeID, Item item
+        // returns:  bool
+        public void editItemDetails(Object list)
+        {
+            return state.editItemDetails(storeID, item);
+        }
+
+        // params: String managerName, int storeID
+        // returns: bool
+        public void appointStoreManager(Object list) //the store manager will receive default permissions(4.9)
+        {
+            return state.appointStoreManager(managerName, storeID);
+        }
+
+        // params: String storeOwnerName, int storeID
+        // returns: 
+        public void appointStoreOwner(Object list)
+        {
+            return state.appointStoreOwner(storeOwnerName, storeID);
+        }
+
+        // params: String managerName, ConcurrentBag<Permissions> permissions
+        // returns: bool
+        public void editManagerPermissions(Object list)
+        {
+            return state.editManagerPermissions(managerName, permissions);
+        }
+
+        // params: String managerName, int storeID
+        // returns: bool
+        public void removeStoreManager(Object list)
+        {
+            return state.removeStoreManager(managerName, storeID);
+        }
+
+        // params: int storeID
+        // returns: ConcurrentDictionary<String, ConcurrentBag<Permissions>>
+        public void getOfficialsInformation(Object list)
+        {
+            return state.getOfficialsInformation(storeID);
+        }
+
+        // params: int storeID
+        // returns: ConcurrentBag<PurchaseInfo>
+        public void getStorePurchaseHistory(Object list) //all the purchases of the store that I manage/own
+        {
+            return state.getStorePurchaseHistory(storeID);
+        }
+
+        // params: NONE
+        // returns: ConcurrentDictionary<String, ConcurrentBag<PurchaseInfo>>
+        public void getUsersPurchaseHistory(Object list)
+        {
+            return state.getUsersPurchaseHistory();
+        }
+
+        // params: NONE
+        // returns: ConcurrentDictionary<int, ConcurrentBag<PurchaseInfo>>
+        public void getStoresPurchaseHistory(Object list)
+        {
+            return state.getStoresPurchaseHistory();
+        }
 
     }
 }
