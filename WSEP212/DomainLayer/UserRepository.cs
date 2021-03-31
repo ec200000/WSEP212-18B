e058c4701773c9834cab1168138fc0c5e26bd6af
iris kronfeld <iris.kronfeld@gmail.com>
2021-03-31 12:19:40 +0300

    User repository updated

--- WSEP212/DomainLayer/UserRepository.cs
+++ WSEP212/DomainLayer/UserRepository.cs
@@ -7,7 +7,44 @@ namespace WSEP212.DomainLayer
 {
     public class UserRepository
     {
-        //singelton
+        private static readonly Lazy<UserRepository> lazy
+        = new Lazy<UserRepository>(() => new UserRepository());
+
+        public static UserRepository Instance
+            => lazy.Value;
+
+        private UserRepository() { }
         public ConcurrentBag<User> users { get; set; }
+        public ConcurrentDictionary<String,String> usersInfo { get; set; }
+
+        public bool insertNewUser(User newUser,String password)
+        {
+            users.Add(newUser);
+            return true;
+        }
+
+        public bool removeUser(User userToRemove)
+        {
+            return users.TryTake(out userToRemove);
+        }
+
+        public bool updateUser(User userToUpdate)
+        {
+            return true;
+        }
+
+        //<param>: String userName
+        //<returns>: If found -> user returned, otherwise null is returned
+        public User findUserByUserName(String userName)
+        {
+            foreach(User user in users)
+            {
+                if(user.userName.Equals(userName))
+                {
+                    return user;
+                }
+            }
+            return null;
+        }
     }
 }
