a9c5b35f407c6ce8bd2e8ec3edd42292de17a5ca
iris kronfeld <iris.kronfeld@gmail.com>
2021-03-31 12:20:40 +0300

    updated authentication

--- WSEP212/DomainLayer/Authentication.cs
+++ WSEP212/DomainLayer/Authentication.cs
@@ -6,6 +6,20 @@ namespace WSEP212.DomainLayer
 {
     public class Authentication
     {
-        //Authentication will be singelton
+        private static readonly Lazy<Authentication> lazy
+        = new Lazy<Authentication>(() => new Authentication());
+
+        public static Authentication Instance
+            => lazy.Value;
+
+        private Authentication() { }
+
+        public String encryptPassword(String password)
+        {
+            byte[] data = System.Text.Encoding.ASCII.GetBytes(password);
+            data = new System.Security.Cryptography.SHA256Managed().ComputeHash(data);
+            String hash = System.Text.Encoding.ASCII.GetString(data);
+            return hash;
+        }
     }
 }
