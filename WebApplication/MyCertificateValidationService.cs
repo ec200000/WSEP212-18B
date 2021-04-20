9fbc915572404b04d5c3c28f5bdc766c578a7fb0
iris kronfeld <iris.kronfeld@gmail.com>
2021-04-20 20:16:42 +0300

    adding certificate for HTTPS

---
+++ WebApplication/MyCertificateValidationService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Extensions.Configuration;
+
+namespace WebApplication2
+{
+    public class MyCertificateValidationService
+    {
+        public IConfiguration Configuration { get; set; }
+
+        public MyCertificateValidationService(IConfiguration config)
+        {
+            Configuration = config;
+        }
+        public bool ValidateCertificate(X509Certificate2 clientCertificate)
+        {
+            var pass = Configuration["certificatePassword"];
+            var cert = new X509Certificate2(Path.GetFullPath("server.crt"), "sadna");
+            if (clientCertificate.Thumbprint == cert.Thumbprint)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
--- WebApplication/obj/Debug/net5.0/WebApplication.AssemblyInfo.cs
+++ WebApplication/obj/Debug/net5.0/WebApplication.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("c14383bc-e11d-411a-8b52-2eb9453698f9")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
