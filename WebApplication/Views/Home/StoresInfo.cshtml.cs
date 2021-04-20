d6e5ea5098ca7f2e7b828c14c7474554090d2914
ec200000 <ec200000@gmail.com>
2021-04-20 20:53:51 +0300

    starting of use case 2.5

---
+++ WebApplication/Views/Home/StoresInfo.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Concurrent;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WSEP212.ConcurrentLinkedList;
+using WSEP212.DomainLayer;
+using WSEP212.ServiceLayer;
+using WSEP212.ServiceLayer.Result;
+
+namespace WebApplication.Views.Home
+{
+    public class StoresInfo : PageModel
+    {
+        public ConcurrentDictionary<Store, ConcurrentLinkedList<Item>> stores { get; set; }
+        
+        public void OnGet()
+        {
+            SystemController systemController = SystemController.Instance;
+            ConcurrentDictionary<Store, ConcurrentLinkedList<Item>> res = systemController.getItemsInStoresInformation();
+            this.stores = res;
+        }
+    }
+}
