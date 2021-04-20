022d2a9ea2b43df66b003cb90e7601a752f169ac
iris kronfeld <iris.kronfeld@gmail.com>
2021-04-20 13:38:16 +0300

    created system manager when the program starts

--- WSEP212-TESTS/AcceptanceTests/StoreTests.cs
+++ WSEP212-TESTS/AcceptanceTests/StoreTests.cs
@@ -10,7 +10,7 @@ namespace WSEP212_TESTS.AcceptanceTests
     [TestClass]
     public class StoreTests
     {
-        SystemController controller = new SystemController();
+        SystemController controller = SystemController.Instance;
         int itemID;
         int storeID;
 
